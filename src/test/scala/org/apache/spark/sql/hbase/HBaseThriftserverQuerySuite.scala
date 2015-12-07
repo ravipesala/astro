8065c7185146b6932be0dcfdb0b1dfa8e1918a27
Ravindra Pesala <ravi.pesala@gmail.com>
2015-12-07 17:29:11 +0800

    Updated the test suite to get user name from system property

--- src/test/scala/org/apache/spark/sql/hbase/HBaseThriftserverQuerySuite.scala
+++ src/test/scala/org/apache/spark/sql/hbase/HBaseThriftserverQuerySuite.scala
@@ -56,7 +56,7 @@ class HBaseThriftserverQuerySuite extends TestBaseWithNonSplitData {
         logError("JDBC Driver not loaded", e)
     }
     Thread.sleep(20000)
-    val con = DriverManager.getConnection("jdbc:hive2://localhost:10000/default", "jenkins", "");
+    val con = DriverManager.getConnection("jdbc:hive2://localhost:10000/default", System.getProperty("user.name"), "");
     stmt = con.createStatement();
 
     val testTableCreationHiveSQL =
